f9a48a4386aa8155a263552846db7d5a119f6550
Pedro Henrique Freitas Ferreira <phfreitasf@gmail.com>
2022-04-07 17:24:48 -0300

    adicionado colisao, sons, botao reiniciar

--- js/flappy.js
+++ js/flappy.js
@@ -35,78 +35,90 @@ function ParDeBarreiras(altura, abertura, x) {
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
     this.setX = x => this.elemento.style.left = `${x}px`
     this.getLargura = () => this.elemento.clientWidth
-   
+
     this.sortearAbertura()
     this.setX(x)
 }
 
 
-    function Barreiras(altura, largura, abertura, espaco, notificarPonto)
-    {
-        this.pares = [
-            new ParDeBarreiras(altura, abertura, largura),
-            new ParDeBarreiras(altura, abertura, largura + espaco),
-            new ParDeBarreiras(altura, abertura, largura + espaco * 2),
-            new ParDeBarreiras(altura, abertura, largura + espaco * 3)
-        ]
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
 
-        const velocidadeBarreira = 5
-        this.animar = () =>
-        {
-            this.pares.forEach(par => {
-                par.setX(par.getX() - velocidadeBarreira)
-                // console.log(`log getLargura ${par.getLargura}`)
-                // console.log(`log getX ${par.getX}`)
+    const velocidadeBarreira = 5
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - velocidadeBarreira)
+            // console.log(`log getLargura ${par.getLargura}`)
+            // console.log(`log getX ${par.getX}`)
 
-                // quando o cano sair da area do jogo
+            // quando o cano sair da area do jogo
 
-                if (par.getX() < -par.getLargura())
-                {
-                    par.setX(par.getX() + espaco * this.pares.length)
-                    par.sortearAbertura()
-                }
+            if (par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbertura()
+            }
 
-                const meio = largura / 2
-                const cruzouMeio = par.getX() + velocidadeBarreira >= meio 
+            const meio = largura / 2
+            const cruzouMeio = par.getX() + velocidadeBarreira >= meio
                 && par.getX() < meio
-                if (cruzouMeio) notificarPonto()
-            });
+            if (cruzouMeio) notificarPonto()
+        });
+    }
+}
+
+
+function BotaoReiniciar(areaDoJogo) {
+    this.elemento = novoElemento('img', 'reiniciar')
+    this.elemento.src = './imgs/reiniciar.png'
+    this.elemento.onclick = function () {
+        while (areaDoJogo.firstChild) {
+            areaDoJogo.firstChild.remove()
+        }
+        new YasuoBird().start()
+    }
+
+}
+
+function Reiniciar(areaDoJogo, botao) {
+    areaDoJogo.replaceChildren('')
+    new YasuoBird().start()
+}
+
+
+function Passaro(alturaJogo) {
+    let voando = false
+
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -6)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0) {
+            this.setY(0)
+        }
+        else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima)
+        }
+        else {
+            this.setY(novoY)
         }
     }
 
-    function Passaro(alturaJogo)
-    {
-        let voando = false
-
-        this.elemento = novoElemento('img','passaro')
-        this.elemento.src = 'imgs/passaro.png'
-
-        this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
-        this.setY = y => this.elemento.style.bottom = `${y}px`
-
-        window.onkeydown = e => voando = true
-        window.onkeyup = e => voando = false
-
-        this.animar = () =>
-        {
-            const novoY = this.getY() + (voando ? 8 : -6)
-            const alturaMaxima = alturaJogo - this.elemento.clientHeight
-
-            if (novoY <= 0)
-            {
-                this.setY(0)
-            }
-            else if (novoY >= alturaMaxima)
-            {
-                this.setY(alturaMaxima)
-            }
-            else {
-                this.setY(novoY)
-            }
-        }
-
-        this.setY(alturaJogo / 2)
-    }
+    this.setY(alturaJogo / 2)
+}
 
 // const barreiras = new Barreiras(800,1200,300,400)
 // const passaro = new Passaro(700)
@@ -123,39 +135,106 @@ function ParDeBarreiras(altura, abertura, x) {
 //     passaro.animar()
 // }, 20)
 
-function Progresso()
-{
-    this.elemento =  novoElemento('span','progresso')
-    this.atualizarPontos = pontos => 
-    {
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
         this.elemento.innerHTML = pontos
+        if (pontos > 0) {
+            const audio = new Audio('./sounds/score.wav')
+            audio.play()
+
+
+        }
     }
+
     this.atualizarPontos(0)
 }
 
-function YasuoBird()
-{
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const ladodireitoA = a.left + a.width
+    const ladodireitoB = b.left + b.width
+
+    const horizontal = ladodireitoA >= b.left && ladodireitoB >= a.left
+
+    const verticalA = a.top + a.height
+    const verticalB = b.top + b.height
+
+    const vertical = verticalA >= b.top && verticalB >= a.top
+
+    return horizontal && vertical
+
+}
+
+function colidiu(yasuo, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+
+            colidiu = estaoSobrepostos(yasuo.elemento, superior) || estaoSobrepostos(yasuo.elemento, inferior)
+
+        }
+
+    })
+    return colidiu
+}
+
+function YasuoBird() {
     let pontos = 0
 
     const areaDoJogo = document.querySelector('[wm-flappy]')
     const altura = areaDoJogo.clientHeight
     const largura = areaDoJogo.clientWidth
     const progresso = new Progresso()
-
-    const barreiras = new Barreiras(800,1200,300,400, () => progresso.atualizarPontos(pontos++))
+    const reiniciar = new BotaoReiniciar(areaDoJogo)
+    const barreiras = new Barreiras(800, 1200, 400, 400, () => progresso.atualizarPontos(pontos++))
     const yasuo = new Passaro(altura)
+    //const tocandomusica = new MusicaDeFundo().getPlaying()
+    
 
     areaDoJogo.appendChild(progresso.elemento)
     areaDoJogo.appendChild(yasuo.elemento)
-    barreiras.pares.forEach( par => areaDoJogo.appendChild(par.elemento))
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
 
     this.start = () => {
-        const temporizador = setInterval(() =>
-        {
+        const temporizador = setInterval(() => {
+
             barreiras.animar()
             yasuo.animar()
+            // console.log(musica.pause = true)
+            
+            if (colidiu(yasuo, barreiras)) {
+                clearInterval(temporizador)
+                areaDoJogo.appendChild(reiniciar.elemento)
+
+            }
         }, 20
-        )}    
+        )
+    }
 }
 
+function MusicaDeFundo() {
+
+    this.musica = new Audio('./sounds/back.mp3')
+    this.musica.loop = true
+    //this.musica.muted = true
+    this.getPlaying = () => this.musica.paused
+    this.tocar = () => this.musica.play()
+
+        
+}
+
+let inicio = 0
+document.addEventListener('keydown', function(event) {
+    const musica = new MusicaDeFundo()
+    if (event.keyCode == 32 &&  inicio == 0){
+        musica.tocar()
+        inicio=1
+    }
+})
 new YasuoBird().start()
+   
